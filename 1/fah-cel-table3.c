db98e7b3f666538edd00e50dbd8f7ac42f3990c9
riceset <48802655+riceset@users.noreply.github.com>
2021-09-02 16:41:09 -0300

    for loop

---
+++ 1/fah-cel-table3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit */
+#define STEP 20     /* step size */
+
+/*print Fahrenheit-Celsius table */
+
+main()
+{
+  for (int fah = LOWER; fah <= UPPER; fah += STEP)
+    printf("%3d\t%6.1f\n", fah, (5.0 / 9.0) * (fah - 32));
+}
