5881daf403d7b36ad1886c8eb10c544837aa7c97
riceset <48802655+riceset@users.noreply.github.com>
2021-09-02 19:51:07 -0300

    copy IO

---
+++ 1/copyIO.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* copy input to output v1 */
+
+int main(void)
+{
+  int c;
+
+  c = getchar();
+
+  while (c != EOF)
+  {
+    putchar(c);
+    c = getchar();
+  }
+}
---
+++ 1/copyIO2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* copy input to output v2 */
+
+int main(void)
+{
+  int c;
+
+  while ((c = getchar()) != EOF)
+    putchar(c);
+}
